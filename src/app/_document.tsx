c6e955d7e707e3ff37ef8599868ddba792003e39
raihansarker270 <raihansarker270@gmail.com>
2025-08-16 04:03:25 +0600

    Update _document.tsx

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -1,1 +1,17 @@
+// _document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
 
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Add the favicon link */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
