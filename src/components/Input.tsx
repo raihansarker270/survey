ea35626b212bbef12659274fc5e75cc78989518f
raihansarker270 <raihansarker270@gmail.com>
2025-08-21 15:11:01 +0000

    Update all changes from Codespace

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,26 @@
-interface InputProps {
-  type?: string;
-  placeholder?: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+import * as React from "react";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  // চাইলে এখানে কাস্টম props যোগ করতে পারো
 }
 
 export default function Input({
   type = "text",
   placeholder = "",
   value,
-  onChange
+  onChange,
+  className,
+  ...props
 }: InputProps) {
   return (
     <input
-      className="input"
+      className={`input ${className ?? ""}`}
       type={type}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
+      {...props} // <-- required, disabled, name, ইত্যাদি সব support হবে
     />
   );
 }
